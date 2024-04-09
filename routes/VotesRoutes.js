007dadf9d3382a720afc273e1f3d48b9613df5ad
harish <harishnagubadi9190@gmail.com>
2024-04-09 10:03:58 +0530

    final-release1

---
+++ routes/VotesRoutes.js
@@ -0,0 +1,23 @@
+
+const express = require("express")
+const router = express.Router()
+
+const voteController = require("../Controllers/VotesControllers")
+const cron = require('node-cron')
+
+
+
+router.post("/vote/:userId", voteController.addVote)
+
+
+
+router.get('/getvotescount', voteController.voteCount)
+
+// This function will run based on the time which I have mentioned
+cron.schedule("0 35 10 * * *", voteController.cronJob)
+
+router.get("/getwinner", voteController.getWinner)
+
+router.get("/getvotes", voteController.getVotes)
+
+module.exports=router
